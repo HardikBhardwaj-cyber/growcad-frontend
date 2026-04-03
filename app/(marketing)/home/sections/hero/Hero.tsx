a3f5fae57c6716fcbf3634485bc486bc9077ebc0
Hardik Bhardwaj <hardikbhardwajhit@gmail.com>
2026-04-03 13:20:17 +0530

    fix 25

--- app/(marketing)/home/sections/hero/Hero.tsx
+++ app/(marketing)/home/sections/hero/Hero.tsx
@@ -44,12 +44,12 @@ export default function Hero() {
       {/* ================= 🚀 CONTENT ================= */}
 
       <div className="relative w-full max-w-[1500px] mx-auto 
-      px-6 md:px-16 lg:px-24 xl:px-32">
+      px-6 md:px-16 lg:px-28 xl:px-40">
 
-        <div className="grid lg:grid-cols-[1.05fr_0.95fr] items-center min-h-[88vh]">
+        <div className="grid lg:grid-cols-[minmax(0,1.1fr)_minmax(0,0.9fr)] items-center min-h-[88vh]">
 
           {/* 🔥 LEFT (SHIFTED RIGHT FOR BREATHING SPACE) */}
-          <div className="flex justify-start pl-2 md:pl-6 lg:pl-10 xl:pl-14">
+          <div className="flex justify-start">
             <div className="max-w-[540px] w-full">
               <HeroLeft />
             </div>
